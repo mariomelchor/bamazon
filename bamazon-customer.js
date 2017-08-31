2c9486dd3125c7c2e993548368d2d39bb6ca8645
mariomelchor <mario@mariomelchor.com>
2017-08-30 22:32:05 -0700

    Passing product.item_id when product to buy is selected, Prompting user for qty, checking enough in stock

--- bamazon-customer.js
+++ bamazon-customer.js
@@ -19,7 +19,7 @@ var showProducts = function() {
     var products = results;
     purchaseProducts(products);
 	});
-}
+};
 
 // Ask user which product to purchase
 var purchaseProducts = function(products) {
@@ -32,11 +32,54 @@ var purchaseProducts = function(products) {
       var productsArray =[];
       // loop through products and return array for choices
       for (var index = 0; index < products.length; index++) {
-        productsArray.push( products[index].product_name +' - $'+ products[index].price );
+        productsArray.push({
+          name:  products[index].product_name +' - $'+ products[index].price,
+          value: products[index].item_id,
+          short: products[index].product_name
+        });
       }
       return productsArray;
     }
   }]).then(function (answers) {
-    console.log(answers.choice);
+
+    for (var index = 0; index < products.length; index++) {
+      if(products[index].item_id === answers.choice) {
+
+        // Store product purchased obj
+        var prodPurchased = products[index];
+        
+        // Ask User how many to buy
+        inquirer.prompt([{
+          name: 'choice',
+          type: 'input',
+          message: "How many would you like to buy?",
+          // Validate if value a number
+          validate: function(value) { 
+            if ( isNaN(value) ) { 
+              console.log('\n Please provide a number'); 
+              return false; 
+            } else if( value <= 0 ) {
+              console.log('\n Please provide a number greater than 0'); 
+              return false; 
+            } else {
+              return true;
+            }
+          },
+        }]).then(function (answers) {
+
+          // Store Qty
+          var prodQty = answers.choice;
+
+          // Check if there's enough in stock
+          if(prodPurchased.stock_quantity >= prodQty) {
+            console.log('There is enough to buy');
+          } else {
+            console.log('Sorry Insufficient quantity!');
+          }
+
+        });
+      }
+    }
+
   });
-}
+};
