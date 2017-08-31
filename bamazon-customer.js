3817c16c87afd6fca720ec7930a398ab82367694
mariomelchor <mario@mariomelchor.com>
2017-08-30 20:45:07 -0700

    Created purchaseProducts function displaying products from DB

--- bamazon-customer.js
+++ bamazon-customer.js
@@ -11,19 +11,32 @@ connection.connect(function(err) {
   showProducts();
 });
 
-
 // Show all Products in DB
 var showProducts = function() {
-
 	connection.query('SELECT * FROM products', function (error, results, fields) {
 	  if (error) throw error;
 	  
     var products = results;
-    
-    // loop through products
-    for (var index = 0; index < products.length; index++) {
-      console.log( products[index].item_id +' - '+ products[index].product_name +' - $'+products[index].price );
-    };
-    
+    purchaseProducts(products);
 	});
+}
+
+// Ask user which product to purchase
+var purchaseProducts = function(products) {
+  inquirer.prompt([{
+    name: 'choice',
+    type: 'list',
+    pageSize: 100,
+    message: "What product would you like to purchase?",
+    choices: function(value){
+      var productsArray =[];
+      // loop through products and return array for choices
+      for (var index = 0; index < products.length; index++) {
+        productsArray.push( products[index].product_name +' - $'+ products[index].price );
+      }
+      return productsArray;
+    }
+  }]).then(function (answers) {
+    console.log(answers.choice);
+  });
 }
